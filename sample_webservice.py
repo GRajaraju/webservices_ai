18c182cd7c703a45ff101ff6aa0ec28164cb8c8e
jungleecode <jungleecode@users.noreply.github.com>
2018-05-01 16:24:42 +0530

    Update sample_webservice.py

--- sample_webservice.py
+++ sample_webservice.py
@@ -1,12 +1,13 @@
 # This is a sample webservice created in Python using flask
 
-from flask import Flask
+from flask import Flask # importing flask
 
-app = Flask(__name__)
+app = Flask(__name__) # creating Flask object
 
-@app.route('/')
+@app.route('/') # '/' specifies root or home page. @app is a decorator to which we are attaching a function.
 def test_service():
-  return "test passed"
+  return "test passed" # on access to '/' in the web browser, the string 'test passed' is returned.
  
-app.run(port=5000)
+app.run(port=5000) # running the web service available at port 5000
+
 
