f14185cc6602bdf1cf50464ce2d14b8c467a62a2
jungleecode <jungleecode@users.noreply.github.com>
2018-05-02 14:42:13 +0530

    Update python_decorators.py

--- python_decorators.py
+++ python_decorators.py
@@ -1,17 +1,17 @@
 # Python decorators are basically objects which are callable and are used to modify a function or a class during runtime.
-# The way the Python decorator work is, the function (also called objects in Python given they are First class objects) is
+# The way the Python decorator works is, the function (also called objects in Python given they are First class objects) is
 # passed to a decorator (the one which is going to modify the original function behaviour) as an argument, the decorator 
-# modifies the function and return it. Here's an example where we have an original function which lets say finds square of 
+# modifies the function and returns it. Here's an example where we have an original function which lets say finds square of a
 # number and we want to modify the function so it returns a cube of a number.
 
 def sqr_num(x):
   return x**2
 
-# Now, we are going to modify this function during runtime to return a cube of a number and an arbitary to the result.
+# Now, we are going to modify this function during runtime to return a cube of a number and add an arbitary number to the result.
 
-def decorator_function(original_function): # decorated function, the one resposible to modify the function and return the modified function.
+def decorator_function(original_function): # decorated function, the one resposible to modify the original function and return the modified version.
   
-  def wrapper_function(x,y): # the real modification happens here, where we are fnding the cube of a number and adding an arbitary number to the result.
+  def wrapper_function(x,y): # the real modification happens here, where we are finding the cube of a number and adding an arbitary number to the result.
     res = 0
     res = original_function(x)
     modified_result = res * x + y
@@ -22,5 +22,5 @@ def decorator_function(original_function): # decorated function, the one resposi
 def sqr_num(x):
     return x**2
 
-print(sqr_num(3,8)) # Observe that orginally the function sqr_num used to accept only one argument, and now post modification is accepts 2 arguments.
+print(sqr_num(3,8)) # Observe that orginally the function sqr_num used to accept only one argument, and now post modification it accepts 2 arguments.
 
